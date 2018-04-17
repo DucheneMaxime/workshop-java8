f01a6710594a8138b0c1e532fbb6f88a27d1f381
Maxime Duchene <duchene.maxime@outlook.fr>
2018-04-17 17:53:53 +0200

    Partie 2 - Exo 3 terminé

--- src/test/java/java8/ex03/Lambda_03_Test.java
+++ src/test/java/java8/ex03/Lambda_03_Test.java
@@ -4,6 +4,8 @@ import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
@@ -22,7 +24,12 @@ public class Lambda_03_Test {
 
     // tag::forEach[]
     private void forEach(List<Person> source, PersonProcessor processor) {
-       // TOD0
+       // TODO
+    	Iterator<Person> it = source.iterator();
+    	while(it.hasNext()){
+    		Person p = (Person) it.next();
+    		processor.process(p);
+    	}
     }
     // end::forEach[]
 
@@ -36,7 +43,14 @@ public class Lambda_03_Test {
         // TODO vérifier qu'une personne à un prénom qui commence par first
         // TODO vérifier qu'une personne à un nom qui commence par last
         // TODO vérifier qu'une personne à un age > 0
-        PersonProcessor verifyPerson = null;
+
+        PersonProcessor verifyPerson = new PersonProcessor(){ 
+	        public void process(Person p){
+	            assertTrue(p.getFirstname().startsWith("first"));
+	            assertTrue(p.getLastname().startsWith("last"));
+	            assertTrue(p.getAge()>0);
+	        }
+        };
 
         assertThat(verifyPerson, notNullValue());
 
